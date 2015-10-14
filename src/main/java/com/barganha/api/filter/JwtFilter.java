74abc07411e51199e30259eef32074b88ebe666c
09893064708 <gustavomr@gmail.com>
2015-10-14 17:29:54 -0300

    Adding server response with headers and response.

--- src/main/java/com/barganha/api/filter/JwtFilter.java
+++ src/main/java/com/barganha/api/filter/JwtFilter.java
@@ -12,6 +12,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @Component
@@ -26,6 +28,12 @@ public class JwtFilter extends GenericFilterBean {
         try {
 
             HttpServletRequest request = (HttpServletRequest) req;
+            HttpServletResponse response = (HttpServletResponse) res;
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
+            response.setHeader("Access-Control-Expose-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
             sessionService.load(request);
             chain.doFilter(req, res);
 
